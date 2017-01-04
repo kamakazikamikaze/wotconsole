0b72e7b35683542d6675425d77feb632e21be134
Kent Coble <coblekent@gmail.com>
2017-01-03 19:01:34 -0700

    Fixed URL

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='WarGaming World of Tanks Console API Wrapper',
     author='Kent Coble',
     author_email='coblekent@gmail.com',
-    url='https://bitbucket.org/kamakazikamikaze/wot-console',
+    url='https://bitbucket.org/kamakazikamikaze/wotconsole',
     license='LICENSE.TXT',
     long_description=open('README.rst').read(),
     packages=['wotconsole'],
